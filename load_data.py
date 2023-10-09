dea7b6bf6a32d812bbc947fba6ca05a57283ff51
Septemberlemon <84148797+Septemberlemon@users.noreply.github.com>
2023-10-09 09:37:51 +0800

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -5,6 +5,7 @@ import numpy as np
 training_images = np.load("training-images.npy")
 training_labels = np.load("training-labels.npy")
 training_data = [(np.reshape(x,(28*28,1)),y) for x,y in zip(training_images, training_labels)]
+
 # 加载测试数据
 test_images = np.load("test-images.npy")
 test_labels = np.load("test-labels.npy")
